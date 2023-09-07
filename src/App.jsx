901cabf0737f0296fc106d6cf507e609f3f48158
SPRHackz <sprhackz003@gmail.com>
2023-09-07 15:45:57 +0530

    The Footer is Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Demo from "./components/Demo";
+import Footer from "./components/Footer";
 import "./App.css";
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <div className="app">
         <Hero />
         <Demo />
+        <Footer/>
       </div>
     </main>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="w-full flex justify-center  items-center">
+      <p className="text-black mb-5 mt-8 capitalize text-base antialiased">
+        &copy; 2023 SPR Hackz
+      </p>
+    </footer>
+  );
+};
+export default Footer;
